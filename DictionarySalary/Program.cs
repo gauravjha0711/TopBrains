59724ca64a37810a7765e1b719a17736dfca4d94
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 00:28:09 +0530

    Dictionary Salary

---
+++ DictionarySalary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // employeeId, salary
+        Dictionary<int, int> DictionarySalary = new Dictionary<int, int>();
+        Console.Write("Enter number of employees: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Enter details for employee {i + 1} (ID Salary): ");
+            string[] inputs = Console.ReadLine().Split(' ');
+            int empId = Convert.ToInt32(inputs[0]);
+            int salary = Convert.ToInt32(inputs[1]);
+            DictionarySalary[empId] = salary;
+        }
+        Console.Write("Enter number of employee Id space separated to get total salary: ");
+        string[] empIdsInput = Console.ReadLine().Split(' ');
+        int totalSalary = 0;
+        foreach (string idStr in empIdsInput)
+        {
+            int empId = Convert.ToInt32(idStr);
+            if (DictionarySalary.ContainsKey(empId))
+            {
+                totalSalary += DictionarySalary[empId];
+            }
+        }
+        Console.WriteLine($"Total Salary: {totalSalary}");
+    }
+}
