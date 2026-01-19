1c9c09049ff3a87285b5efc92f1583d6bd2b9259
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 00:00:38 +0530

    Inventory Name Cleanup

---
+++ InventoryNameCleanup/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter product name: ");
+        string productName = Console.ReadLine();
+        string cleanedName = CleanProductName(productName);
+        Console.WriteLine("Cleaned Product Name: " + cleanedName);
+    }
+    public static string CleanProductName(string name)
+    {
+        string trimmedName = name.Trim();
+        string ans = "";
+        foreach(char c in trimmedName)
+        {
+            if(ans.Length == 0 || ans[ans.Length-1] != c)
+            {
+                ans += c;
+            }
+        }
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string finalResult = textInfo.ToTitleCase(ans.ToString());
+        return finalResult;
+    }
+}
