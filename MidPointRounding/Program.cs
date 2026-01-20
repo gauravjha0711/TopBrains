9fce82717e4110aeb86cb07d8cb0adebbed4ef71
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 10:13:10 +0530

    Radius round up to 2

---
+++ MidPointRounding/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * Math.Pow(radius, 2);
+        Console.WriteLine($"Area of circle with radius {radius} is: {Math.Round(area,2)}");
+    }
+}
