4cf18350956749975fdbab693fc6e8bcb3161ec8
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 12:08:00 +0530

    Multiplication Table

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter upto: ");
+        int upto = Convert.ToInt32(Console.ReadLine());
+        List<int> table = new List<int>();
+        for(int i = 1; i <= upto; i++)
+        {
+            table.Add(n * i);
+        }
+        foreach(var item in table)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
