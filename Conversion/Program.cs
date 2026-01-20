34a43cdbcb6cd3125c63713a6fed6e599eedbe84
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 10:19:00 +0530

    Conversion feet into cm

---
+++ Conversion/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter feet convert into cm: ");
+        int feet = Convert.ToInt32(Console.ReadLine());
+        double cm = feet * 30.48;
+        Console.WriteLine($"{feet} feet is equal to {Math.Round(cm,2)} cm");
+    }
+}
