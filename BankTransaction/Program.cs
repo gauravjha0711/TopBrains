b56b3209d316f5304e73a86475a0c995d926e9b8
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 12:43:35 +0530

    BankTransaction

---
+++ BankTransaction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter Initial Balance: ");
+        double initialBalance = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter number of transactions: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < n; i++)
+        {
+            Console.Write("Enter transaction amount (positive for deposit, negative for withdrawal): ");
+            double transaction = Convert.ToDouble(Console.ReadLine());
+            if(transaction < 0 && Math.Abs(transaction) > initialBalance)
+            {
+                Console.WriteLine("Insufficient funds for this withdrawal.");
+                return;
+            }
+            else
+            {
+                initialBalance += transaction;
+            }
+        }
+        Console.WriteLine($"Final Balance: {initialBalance}");
+    }
+}
