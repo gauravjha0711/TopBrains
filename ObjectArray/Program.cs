602a264981f6b2d32db1666f08119ff3761c82bb
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 12:55:54 +0530

    ObjectArray

---
+++ ObjectArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+public class Program
+{
+    public static void Main()
+    {
+        ArrayList arrayList = new ArrayList();
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < n; i++)
+        {
+            Console.Write($"Enter element {i + 1}: ");
+            String element = Console.ReadLine();
+            arrayList.Add(element);
+        }
+        int sum = 0;
+        foreach(var item in arrayList)
+        {
+            if(int.TryParse(item.ToString(), out int number))
+            {
+                sum += number;
+            }
+        }
+        Console.WriteLine($"Sum of integer elements: {sum}");
+    }
+}
