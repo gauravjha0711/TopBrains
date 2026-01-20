bd7095531f99d14494c93732684c45a00785e344
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 11:13:38 +0530

    Display height

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter height in cm: ");
+        int height = Convert.ToInt32(Console.ReadLine());
+        if(0<=height && height <= 150)
+        {
+            Console.WriteLine("Short");
+        }
+        else if(151<=height && height <=180)
+        {
+            Console.WriteLine("Average");
+        }
+        else if(181<=height && height <=300)
+        {
+            Console.WriteLine("Tall");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+}
