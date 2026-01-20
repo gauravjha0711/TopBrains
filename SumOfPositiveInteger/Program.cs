2c2aed4355afb13f4c4ab8f61bb6fa423d125bde
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 11:39:50 +0530

    Sum Of Positive Integer

---
+++ SumOfPositiveInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int sum=0;
+        while (true)
+        {
+            Console.Write("Enter number to add, and 0 for stop: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num > 0)
+            {
+                sum += num;
+                Console.WriteLine($"Current sum is: {sum}");
+            }
+            else if (num < 0)
+            {
+                Console.Write("Negative number ignored. ");
+                Console.WriteLine($"Current sum is: {sum}");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine($"Final sum of positive integers is: {sum}");
+                return;
+            }
+        }
+    }
+}
