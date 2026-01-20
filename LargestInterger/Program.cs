cc9260653318b05184abd94fb6556555335179cb
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-20 11:19:03 +0530

    Largest Number

---
+++ LargestInterger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        if(a>b && a>c)
+        {
+            Console.WriteLine($"Largest integer is: {a}");
+        }
+        else if(b>a && b>c)
+        {
+            Console.WriteLine($"Largest integer is: {b}");
+        }
+        else
+        {
+            Console.WriteLine($"Largest integer is: {c}");
+        }
+    }
+}
